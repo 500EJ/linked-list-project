16e60bd8704e4ddcf4155c04a83339ee154844b3
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-30 18:10:49 -0400

    Implement queue

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -4,16 +4,37 @@ class Queue {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
   }
 
   enqueue(val) {
     // Add node to end of queue (linked list)
-    // Write your hypothesis on the time complexity of this method here
+    this.length++;
+    const node = new SinglyLinkedNode(val);
+    if (!this.tail) {
+      this.head = node;
+      this.tail = node;
+      return this.length;
+    }
+    this.tail.next = node;
+    this.tail = node;
+    return this.length;
+    // Time Complexity: O(1)
   }
 
   dequeue() {
     // Remove node from front of queue (linked list)
-    // Write your hypothesis on the time complexity of this method here
+    if (!this.length) return null;
+    this.length--;
+    const deleted = this.head;
+    if (!this.length) {
+      this.head = null;
+      this.tail = null;
+      return deleted.value;
+    }
+    this.head = this.head.next;
+    return deleted.value;
+    // Time Complexity: O(1)
   }
 }
 
